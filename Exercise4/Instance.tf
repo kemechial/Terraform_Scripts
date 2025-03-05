026cf59417c94404f124ccb0acb5357a065c5490
kemechial <kaanevran@gmail.com>
2025-03-05 18:33:36 +0300

    Terraform provisioning with shell script

---
+++ Exercise4/Instance.tf
@@ -0,0 +1,50 @@
+resource "aws_instance" "web" {
+  ami                    = var.amiID[var.region]
+  instance_type          = "t3.micro"
+  key_name               = "dove-key"
+  vpc_security_group_ids = [aws_security_group.dove-sg.id]
+  availability_zone      = var.zone
+
+  tags = {
+    Name    = "dove-web"
+    Project = "dove"
+  }
+
+  provisioner "file" {
+    source      = "web.sh"
+    destination = "/tmp/web.sh"
+  }
+  connection {
+    type        = "ssh"
+    user        = var.webuser
+    private_key = file("./dovekey")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/web.sh",
+      "sudo /tmp/web.sh"
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> web_ip.txt"
+  }
+
+}
+
+resource "aws_ec2_instance_state" "web-state" {
+  instance_id = aws_instance.web.id
+  state       = "running"
+}
+
+output "WebPublicIP" {
+  description = "Public IP of the web instance"
+  value       = aws_instance.web.public_ip
+}
+
+output "WebPrivateIP" {
+  description = "Public IP of the web instance"
+  value       = aws_instance.web.private_ip
+}
